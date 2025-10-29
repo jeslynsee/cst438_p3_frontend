5c393505cd7eae5918b12b2d338c66729a5b384d
jeslynsee <jeslynesee@gmail.com>
2025-10-28 20:29:13 -0700

    Set up basic layout of sign up page and add basic error check for fields. Ready to connect to backend and reroutes to landing page

--- app/sign-up.tsx
+++ app/sign-up.tsx
@@ -1,36 +1,184 @@
+import { useRouter } from "expo-router";
+import { useState } from "react";
 import {
+    ScrollView,
+    StyleSheet,
     Text,
     TextInput,
-    View
+    TouchableOpacity,
+    View,
 } from 'react-native';
+import Header from "./components/Header";
 
 export default function SignUp() {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [errorMsg, setErrorMsg] = useState("");
+    const router = useRouter();
+
+    const handleSignUp = () => {
+        setErrorMsg("");
+        // standard error checks: are fields empty?
+        if (username.length === 0 || email.length === 0 || password.length === 0) {
+            setErrorMsg("All fields must be filled.");
+        } else {
+            // standard errors avoided, so now check first if user exists 
+            // if user exists, set another error message
+            // else reroute to feed/home page, which is our landing page
+            router.replace("/feed");
+        }
+       
+
+    }
+
     return (
-        <View style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "center"
-        }}>
+        <View style={styles.container}>
+
+        <ScrollView contentContainerStyle={styles.scrollContent}>
+
+        <Header/>
+
+        <View style={styles.titleContainer}>
+        <Text style={{
+            fontSize: 30
+        }}> 
+        Sign Up 
+        </Text>
+        </View>
 
             {/* Header */}
-            <Text> Sign Up </Text>
 
             {/* Sign Up form fields */}
-            <View> 
-                <Text>Username: </Text>
-                <TextInput/> 
+
+             {/* Username */}
+          <View style={styles.formContainer}>
+            {/* Username Input */}
+            <View style={styles.inputWrapper}>
+              <Text style={styles.inputLabel}>Username</Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder=""
+                  placeholderTextColor="gray"
+                  value={username}
+                  onChangeText={setUsername}
+                  autoCapitalize="none"
+                  autoComplete="username"
+                />
+              </View>
             </View>
 
-            <View> 
-                <Text>Email: </Text>
-                <TextInput/> 
+             {/* Email */}
+          <View style={styles.formContainer}>
+            {/* Email Input */}
+            <View style={styles.inputWrapper}>
+              <Text style={styles.inputLabel}>Email</Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder=""
+                  placeholderTextColor="gray"
+                  value={email}
+                  onChangeText={setEmail}
+                  autoCapitalize="none"
+                  autoComplete="email"
+                />
+              </View>
             </View>
 
-            <View> 
-                <Text>Password: </Text>
-                <TextInput/> 
+            {/* Password Input */}
+            <View style={styles.inputWrapper}>
+              <Text style={styles.inputLabel}>Password</Text>
+              <View style={styles.inputContainer}>
+                <TextInput
+                  style={styles.input}
+                  placeholder=""
+                  placeholderTextColor="gray"
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={true}
+                  autoCapitalize="none"
+                  autoComplete="password"
+                />
+              </View>
             </View>
+
+            {/* Sign Up */}
+            <TouchableOpacity style={styles.signUpButton} onPress={handleSignUp}>
+              <Text style={styles.signUpButtonText}>Sign up</Text>
+            </TouchableOpacity>
+            {/* Error Message */}
+            <Text style={{color: "red", fontSize: 20}}> {errorMsg} </Text>
+            
+          </View>
+
+        </View>
+
+        </ScrollView>
 
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#C4A484",
+    },
+    titleContainer: {
+        alignItems: "center",
+        justifyContent: "center",
+       marginBottom: 20
+    },
+    scrollContent: {
+        flexGrow: 1,
+        paddingHorizontal: 48,
+        paddingTop: 60,
+        paddingBottom: 40,
+      },
+      formContainer: {
+        marginBottom: 40,
+      },
+      inputWrapper: {
+        marginBottom: 24,
+      },
+      inputLabel: {
+        position: 'absolute',
+        top: -20,
+        paddingHorizontal: 10,
+        fontSize: 16,
+        fontWeight: '500',
+        color: '#263238',
+        zIndex: 1,
+        // fontStyle: 'bold'
+      },
+      inputContainer: {
+        height: 56,
+        borderWidth: 1,
+        borderColor: 'rgba(38, 50, 56, 0.16)',
+        borderRadius: 8,
+        backgroundColor: '#FFFFFF',
+      },
+      input: {
+        flex: 1,
+        paddingHorizontal: 12,
+        fontSize: 16,
+        color: 'rgba(38, 50, 56, 0.6)',
+        letterSpacing: 0.16,
+      },
+      signUpButton: {
+        height: 50,
+        backgroundColor: "#3B1F12",
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 16,
+      },
+      signUpButtonText: {
+        color: '#FFFFFF',
+        fontSize: 18,
+        fontWeight: '400',
+      },
+});
