02e51e96db1ad2a447a48c152ebc6b7030e4f120
jeslynsee <jeslynesee@gmail.com>
2025-11-04 19:09:02 -0800

    Create context folder to add userContext and useStorageState files from previous project. These files will help with user persistence. Changed user context file to grab full user object so we have access to user info

---
+++ app/context/userContext.tsx
@@ -0,0 +1,40 @@
+//@ts-nocheck
+import { createContext, useContext, type PropsWithChildren } from "react";
+import { useStorageState } from "./useStorageState";
+
+const UserContext = createContext({
+  signIn: async (user: any) => {},  // store full user object
+  signOut: () => {},
+  session: null, // will hold the user object
+  isLoading: false,
+});
+
+export function useSession() {
+  const value = useContext(UserContext);
+  if (!value) throw new Error("useSession must be wrapped in a <SessionProvider />");
+  return value;
+}
+
+export function SessionProvider({ children }: PropsWithChildren) {
+  const [[isLoading, session], setSession] = useStorageState("session");
+
+  return (
+    <UserContext.Provider
+      value={{
+        signIn: async (user: any) => {
+          // store the full user object as session because we will need user info to display and persist
+          setSession(JSON.stringify(user)); // convert object to string for storage
+        },
+        signOut: async () => {
+          setSession(null);
+        },
+        // parsing user object because when sending into local storage (web) or secure store (mobile),
+        // it only stores Strings
+        session: session ? JSON.parse(session) : null,
+        isLoading,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
